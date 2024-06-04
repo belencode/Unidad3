89bd7fbbafcf0656452a4e39e897df65d7abbee4
belencode <140363197+belencode@users.noreply.github.com>
2024-06-04 00:52:55 -0400

    Update scripts.js
    Se incluye el cambio de imagen de servicio

--- Anterior Amanda/ENERGY_AND_WATER/scripts.js
+++ Anterior Amanda/ENERGY_AND_WATER/scripts.js
@@ -17,7 +17,7 @@ function mostrarInfo(id, boton) {
     // Agregar la clase 'activo' al botón seleccionado
     boton.classList.add('activo');
 }
-function mostrarInfoServicios(id, boton) {
+function mostrarInfoServicios(id, boton,imagen) {
     // Ocultar todos los elementos con la clase 'info'
     var infos = document.querySelectorAll('.infoServicios');
     infos.forEach(function(infoServicios) {
@@ -35,6 +35,10 @@ function mostrarInfoServicios(id, boton) {
     });
     // Agregar la clase 'activo' al botón seleccionado
     boton.classList.add('activo');
+
+    //modificar la imagen según el boton seleccionado
+    var img = document.getElementById('imagen-servicio');
+    img.src = imagen;
 }
 // Función para mostrar información inicial al cargar la página
 document.addEventListener("DOMContentLoaded", function() {
@@ -45,6 +49,6 @@ document.addEventListener("DOMContentLoaded", function() {
 
     var primerBotonServicio = document.querySelector('.boton-servicios');
     if (primerBotonServicio) {
-        mostrarInfoServicios('infoServicio1', primerBotonServicio); // Mostrar inicialmente la información del primer botón de servicios
+        mostrarInfoServicios('infoServicio1', primerBotonServicio, 'https://www.energyandwater.cl/gallery_gen/3cf1905198e9763be7cf8a3276a8375b_559x1125_79x0_771x1125_crop.jpg?ts=1702866984'); // Mostrar inicialmente la información del primer botón de servicios
     }
 });
